36e37eec9c91eec0ae000583c5504ce479bfc1f3
Rob <r.p.ivie@gmail.com>
2021-04-26 15:18:59 -0600

    fixed stylings on signup

--- src/pages/GetStarted.js
+++ src/pages/GetStarted.js
@@ -1,8 +1,7 @@
 import "./GetStarted.css";
-import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { Button, Col, Form, Row } from "react-bootstrap";
 
 function GetStarted() {
   return (
@@ -24,12 +23,16 @@ function GetStarted() {
           aria-label="confirm"
         />
       </Form.Group>
-      <Form.Group>
-        <Form.Check
-          type="checkbox"
-          label="I have read and agree to the Terms of Service and Privacy Policy"
-        />
-      </Form.Group>
+
+      <Form.Check controlId="formBasicCheckbox">
+        <Form.Check.Input type="checkbox" />
+        <small>
+          <Form.Check.Label>
+            I have read and agree to the Terms of Service and Privacy Policy.
+          </Form.Check.Label>
+        </small>
+      </Form.Check>
+      <br />
       <Button href="/welcome" variant="primary" size="lg" block>
         Get Started
       </Button>
@@ -46,11 +49,47 @@ function GetStarted() {
         </Col>
       </Row>
       <br />
-      <Button href="/welcome" variant="primary" size="lg" block>
-        Continue with Facebook
+      <Button
+        href="/welcome"
+        variant="dark"
+        size="lg"
+        block
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          backgroundColor: "#4267B2",
+          border: "none",
+        }}
+      >
+        <span>
+          <FontAwesomeIcon size="1x" icon={faFacebook} />
+        </span>
+        <span
+          style={{ display: "flex", justifyContent: "center", width: "100%" }}
+        >
+          Continue with Facebook
+        </span>
       </Button>
-      <Button href="/welcome" variant="primary" size="lg" block>
-        Continue with Google
+      <Button
+        href="/welcome"
+        variant="dark"
+        size="lg"
+        block
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          backgroundColor: "#4285F4",
+          border: "none",
+        }}
+      >
+        <span>
+          <FontAwesomeIcon size="1x" icon={faGoogle} />
+        </span>
+        <span
+          style={{ display: "flex", justifyContent: "center", width: "100%" }}
+        >
+          Continue with Google
+        </span>
       </Button>
     </Form>
   );
